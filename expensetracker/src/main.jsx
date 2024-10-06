4affb386d9fd59036cab12ea37684cebfe45f2e4
gautamKumar <gautamkannoujiya58@gmail.com>
2024-10-07 02:57:06 +0530

    vercel deployment failure

--- expensetracker/src/main.jsx
+++ expensetracker/src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// commet to test the import
 import "./index.css";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
