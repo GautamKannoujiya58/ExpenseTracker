f9a88a8064175dee85d549d7fa3189d7dc04b33b
gautamKumar <gautamkannoujiya58@gmail.com>
2024-10-07 00:52:42 +0530

    Pagination logic implemented

--- expensetracker/src/components/RecentTransactions/RecentTransactions.jsx
+++ expensetracker/src/components/RecentTransactions/RecentTransactions.jsx
@@ -4,6 +4,8 @@ import { CiRollingSuitcase } from "react-icons/ci";
 import { GiPayMoney } from "react-icons/gi";
 import TopExpenses from "../TopExpenses/TopExpenses";
 import PropTypes from "prop-types";
+// import Pagination from "../Pagination/Pagination";
+import { useState } from "react";
 
 function RecentTransactions({
   expensesList,
@@ -19,11 +21,40 @@ function RecentTransactions({
 
   // reversed the expenseList to show the last entered trasaction first in the row
   const reversedExpensesList = [...expensesList].reverse();
+
+  // Pagination implementation below
+  // Per expense to be displayed on the page
+  const expensePerPage = 3;
+
+  // For setting the current page value
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // For calculating what no of pages can be there
+  const totalPages = Math.ceil(reversedExpensesList.length / expensePerPage);
+
+  // slicing the first three of the next 3 transactions
+  const currentExpenseToDisplay = reversedExpensesList.slice(
+    (currentPage - 1) * expensePerPage,
+    currentPage * expensePerPage
+  );
+  // console.log("currentExpenseToDisplay", currentExpenseToDisplay);
+
+  // Button click event handling
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  const handlePrev = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
   // console.log("reversedExpenseList >>>", reversedExpensesList);
 
   return (
     <>
-      {reversedExpensesList.map((list) => (
+      {currentExpenseToDisplay.map((list) => (
         <div key={list.id} id={list.id}>
           {categoryIcons[list.category] || <GiPayMoney />}
           <p>{list.title}</p>
@@ -37,6 +68,8 @@ function RecentTransactions({
           </button>
         </div>
       ))}
+      <button onClick={handlePrev}>⬅️Prev</button>
+      <button onClick={handleNext}>➡️Next</button>
       <br></br>
       <TopExpenses expensesList={expensesList} />
       <br></br>
